1283cdc5d43c603483f25878ac22496c83066b63
Kirill <xenonn20@gmail.com>
2019-11-09 09:02:21 +0400

    Initial Commit

---
+++ Parallax Effect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parallax Effect
+//
+//  Created by Кирилл Медведев on 09.11.2019.
+//  Copyright © 2019 Kirill Medvedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
